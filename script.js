aa760e3557ee60165988e2c205fd0ef83cfe88c3
oreoMc <116703604+oreoMc@users.noreply.github.com>
2023-03-22 20:48:32 -0400

    Initial commit

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const jsConfetti = new JSConfetti();
 
 button.addEventListener('click', () => {
   jsConfetti.addConfetti({
-    confettiRadius: 8,
-    confettiNumber: 900,
+    confettiRadius: 6,
+    confettiNumber: 800,
     //emojis: ['🌈'],
     confettiColors: ['#ff0000', '#ff7300', '#ffbb00', '#4dff00', '#0099ff', '#ff00de'],
   });
